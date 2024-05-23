d3226854f1ace50dc4165a21626a50ea31e8c572
Kossatyy Maxim <maxim.k@halykmarket.com>
2024-05-23 15:22:19 +0500

    dev

--- src/main/java/com/booking_service/BookingServiceApplication.java
+++ src/main/java/com/booking_service/BookingServiceApplication.java
@@ -8,6 +8,7 @@ public class BookingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceApplication.class, args);
+		System.out.println("Hello, World!");
 	}
 
 }
